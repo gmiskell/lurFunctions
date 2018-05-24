5ef0f154b0431457d7d5d7f42d1e0e1df3e6ff51
gmiskell <georgia.miskell@gmail.com>
2018-05-24 15:21:17 +1200

    updates

--- R/cal_change.R
+++ R/cal_change.R
@@ -1,5 +1,5 @@
 #' Calibration Interpolation Over Time.
-#' 
+#'
 #' A function for converting the linear coefficients over time to be weighted based on its proximity to one of the calibrations. The output is the corrected obs value.
 #' @param obs The column under investigation.
 #' @param first.offset The intercept from the first calibration.
@@ -11,18 +11,25 @@
 #' changingCalFUN()
 
 
-changingCalFUN <- function(x, obs, a1, b1, a2, b2){
-  
+changingCalFUN <- function(x, obs, first.offset, second.offset, first.slope, second.slope){
+
   x <- as.data.frame(x)
-  x$obs <- x[, obs]
-  
+  x$obs <- x[, obs];
+  x$first.offset <- x[, first.offset]
+  x$first.slope <- x[, first.slope]
+  x$second.offset <- x[, second.offset]
+  x$second.slope <- x[, second.slope]
+
   # determine the monitoring length based on the length of the obs
   monitoring_length <- length(x$obs)
   x$seq <- seq(1:monitoring_length)
-  
+
   # corrected obs value
-  x$cal_obs <- (first.offset + x$seq*((second.offset - first.offset)/monitoring_length) + (first.slope + x$seq*((second.slope - first.slope)/monitoring_length))*x$obs)
+  x$cal_obs <- (x$first.offset + x$seq*((x$second.offset - x$first.offset)/monitoring_length) + (x$first.slope + x$seq*((x$second.slope - x$first.slope)/monitoring_length))*x$obs)
   x$seq <- NULL
- 
-   return(x)
+  x$first.offset <- NULL
+  x$second.offset <- NULL
+  x$first.slope <- NULL
+  x$second.slope <- NULL
+   x
 }
